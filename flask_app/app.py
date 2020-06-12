10b023324281e5fe9eaf21666d57547e5126a19a
Rajtilak Indrajit <indrajit@springboard.com>
2020-06-11 21:23:49 -0700

    First commit of petrichor flask APP UI
    Co-authored-by: email4srk@gmail.com <email4srk@gmail.com>

---
+++ flask_app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    query = request.args.get('query')
+    if query != "":
+        return render_template("result.html")    
+    return render_template("home.html")
+
+@app.route('/chess')
+def chess():
+    return render_template("chess.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
