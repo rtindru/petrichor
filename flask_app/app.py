a451f83a949b46683081a0589adc890710200ae3
Rajtilak Indrajit <indrajit@springboard.com>
2020-06-13 22:47:58 -0700

    Dynamic template with mock result
    Co-authored-by: email4srk@gmail.com <email4srk@gmail.com>

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,14 +6,28 @@ app = Flask(__name__)
 def home():
     query = request.args.get('query')
     if query:
-        return render_template("result.html")    
+        res = resML(query)
+        return render_template("result.html", result=res)    
     return render_template("home.html")
+
+def resML(query):
+    """
+    This takes in the query from the user and return top 5 words and meaning
+    INPUT: string query
+    OUTPUT: list 5 tuples [('word', 'meaning')]
+    """
+    res = [
+        ('petrichor', 'smell of mud after rain'),
+        ('rain', 'drops of water in sky'),
+        ('water', 'chemical consisting of hydrogen and oxygen'),
+        ('sand','particles that sedimented from rocks'),
+        ('mud', 'layer of soil containing dirt, rocks and other stuff'),
+    ]
+    return res
 
 @app.route('/chess')
 def chess():
     return render_template("chess.html")
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
